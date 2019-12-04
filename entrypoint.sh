0600a4a8c52023710801569ed5cef8283bd91e36
Jordane <contact@jordane.net>
2019-12-04 15:15:33 +0100

    fix ssh key

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 eval $(ssh-agent -s)
 echo -e "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 mkdir -p $GITHUB_WORKSPACE/.ssh
-echo $SSH_PRIVATE_KEY > $GITHUB_WORKSPACE/.ssh/id_rsa_deployer
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' > $GITHUB_WORKSPACE/.ssh/id_rsa_deployer
 chmod 600 $GITHUB_WORKSPACE/.ssh/id_rsa_deployer
 echo "IdentityFile $GITHUB_WORKSPACE/.ssh/id_rsa_deployer\n" >> $GITHUB_WORKSPACE/.ssh/config
 
